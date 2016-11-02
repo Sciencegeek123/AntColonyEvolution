e57952d2c1b0d537f96949985053c6c58dc994ab
W. Craig Jones <Sciencegeek123@hotmail.com>
2016-11-01 21:40:27 -0500

    Cleaned up environment some.

--- AntColonyEvolution/Environment/Environment.h
+++ AntColonyEvolution/Environment/Environment.h
@@ -9,57 +9,65 @@ public:
 	Environment(fuint seed);
 	void ResetEnvironment();
 
-	TileType getType(fushort x, fushort y);
-	uint8_t getDifficulty(fushort x, fushort y);
-	template <class T> T getTile();
+	TileType getType(fuint x, fuint y);
+	uint8_t getDifficulty(fuint x, fuint y);
+	template <class T> T getTile(fuint y, fuint x);
+	std::string PrettyPrint();
 
 	//Later these classes will have more information & actions associated with them.
 	class Tile { 
 		friend Environment;
-	private:
-		fbyte difficulty;
-		TileType type;
 	public:
+		fbyte difficulty;
+		const TileType type = TileType::EMPTY;
 		Tile(fbyte d, TileType t) : difficulty(d), type(t) { }
 	};
 
 	class ColonyTile : public Tile {
 	public:
+		const TileType type = TileType::Colony;
 		ColonyTile(fbyte d, TileType t) : Tile(d, t) { }
 	};
 
 	class GrassTile : public Tile {
 	public:
+		const TileType type = TileType::Grass;
 		GrassTile(fbyte d, TileType t) : Tile(d, t) { }
 	};
 
 	class FoodTile : public Tile {
 	public:
+		const TileType type = TileType::Food;
 		FoodTile(fbyte d, TileType t) : Tile(d, t) { }
 	};
 
 	class PlantTile : public Tile {
 	public:
+		const TileType type = TileType::Plant;
 		PlantTile(fbyte d, TileType t) : Tile(d, t) { }
 	};
 
 	class HazardTile : public Tile {
 	public:
+		const TileType type = TileType::Hazard;
 		HazardTile(fbyte d, TileType t) : Tile(d, t) { }
 	};
 
 	class DangerTile : public Tile {
 	public:
+		const TileType type = TileType::Danger;
 		DangerTile(fbyte d, TileType t) : Tile(d, t) { }
 	};
 
 	class DebrisTile : public Tile {
 	public:
+		const TileType type = TileType::Debris;
 		DebrisTile(fbyte d, TileType t) : Tile(d, t) { }
 	};
 
 	class WallTile : public Tile {
 	public:
+		const TileType type = TileType::Wall;
 		WallTile(fbyte d, TileType t) : Tile(d, t) { }
 	};
 
--- AntColonyEvolution/Environment/Environment_Accessors.cpp
+++ AntColonyEvolution/Environment/Environment_Accessors.cpp
@@ -1,6 +1,7 @@
 #include "Environment.h"
 #include <random>
 #include <vector>
+#include <sstream>
 
 //Later Perlin Noise should be implemented.
 //Simple randomness will be used for now;
@@ -68,34 +69,96 @@ Environment::Environment(fuint seed) {
 		case TileType::Colony:
 			tiles[p] = std::move(std::shared_ptr<Tile>(new ColonyTile(dMap[p], tMap[p])));
 			if (!ColonyA) {
-				ColonyA = std::move(std::shared_ptr<ColonyTile>(*(std::shared_ptr<ColonyTile>*)(&tiles[p])));
+				ColonyA = std::move(std::dynamic_pointer_cast<ColonyTile>(tiles[p]));
 			} else if(!ColonyB) {
-				ColonyB = std::move(std::shared_ptr<ColonyTile>(*(std::shared_ptr<ColonyTile>*)(&tiles[p])));
+				ColonyB = std::move(std::dynamic_pointer_cast<ColonyTile>(tiles[p]));
 			}
 			else {
 				//Error.
 			}
 			break;
 		default:
+			//Error.
 			tiles[p] = std::move(std::shared_ptr<Tile>(new Tile(dMap[p], tMap[p])));
 			break;
 		}
 	}
 }
 
-TileType Environment::getType(fushort x, fushort y) {
-
+TileType Environment::getType(fuint x, fuint y) {
+	return tiles[x + y * DEF_SIM_MAP_SIDE]->type;
 }
 
-uint8_t Environment::getDifficulty(fushort x, fushort y) {
-	
+uint8_t Environment::getDifficulty(fuint x, fuint y) {
+	return tiles[x + y * DEF_SIM_MAP_SIDE]->difficulty;
 }
 
-template <class T> T Environment::getTile() {
-
+template <class T> std::shared_ptr<T> Environment::getTile(fuint x, fuint y) {
+	if (x >= DEF_SIM_MAP_SIDE || y >= DEF_SIM_MAP_SIDE) {
+		return std::move(std::shared_ptr<Tile>(nullptr));
+	}
+	else {
+		return std::move(std::dynamic_pointer_cast<T>(tiles[x + y * DEF_SIM_MAP_SIDE]));
+	}
 }
 
 
 void Environment::ResetEnvironment() {
+	for (fuint p = 0; p < DEF_SIM_MAP_SIZE; p++) {
+		switch (tMap[p])
+		{
+		case TileType::Grass:
+			tiles[p] = std::move(std::shared_ptr<Tile>(new GrassTile(dMap[p], tMap[p])));
+			break;
+		case TileType::Food:
+			tiles[p] = std::move(std::shared_ptr<Tile>(new FoodTile(dMap[p], tMap[p])));
+			break;
+		case TileType::Plant:
+			tiles[p] = std::move(std::shared_ptr<Tile>(new PlantTile(dMap[p], tMap[p])));
+			break;
+		case TileType::Hazard:
+			tiles[p] = std::move(std::shared_ptr<Tile>(new HazardTile(dMap[p], tMap[p])));
+			break;
+		case TileType::Danger:
+			tiles[p] = std::move(std::shared_ptr<Tile>(new DangerTile(dMap[p], tMap[p])));
+			break;
+		case TileType::Debris:
+			tiles[p] = std::move(std::shared_ptr<Tile>(new DebrisTile(dMap[p], tMap[p])));
+			break;
+		case TileType::Wall:
+			tiles[p] = std::move(std::shared_ptr<Tile>(new WallTile(dMap[p], tMap[p])));
+			break;
+		case TileType::Colony:
+			tiles[p] = std::move(std::shared_ptr<Tile>(new ColonyTile(dMap[p], tMap[p])));
+			if (!ColonyA) {
+				ColonyA = std::move(std::dynamic_pointer_cast<ColonyTile>(tiles[p]));
+			}
+			else if (!ColonyB) {
+				ColonyB = std::move(std::dynamic_pointer_cast<ColonyTile>(tiles[p]));
+			}
+			else {
+				//Error.
+			}
+			break;
+		default:
+			//Error.
+			tiles[p] = std::move(std::shared_ptr<Tile>(new Tile(dMap[p], tMap[p])));
+			break;
+		}
+	}
+}
 
+std::string Environment::PrettyPrint() {
+	std::stringstream sstr;
+	for (fint x = 0; x < DEF_SIM_MAP_SIDE; x++) {
+		for (fint y = 0; y < DEF_SIM_MAP_SIDE; y++) {
+			sstr << getDifficulty(x, y);
+		}
+		sstr << "\t#\t";
+		for (fint y = 0; y < DEF_SIM_MAP_SIDE; y++) {
+			sstr << getType(x, y);
+		}
+		sstr << "\n";
+	}
+	return std::move(sstr.str());
 }

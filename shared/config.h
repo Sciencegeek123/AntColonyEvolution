5727617af5f9bc70cb2f3a2f8df72f042412cd8c
W. Craig Jones <Sciencegeek123@hotmail.com>
2016-11-29 07:41:50 -0600

    README

---
+++ shared/config.h
@@ -0,0 +1,17 @@
+/*
+
+THIS FILE IS TO ONLY HAVE DEFINE STATEMENTS.
+Later we can make this dynamic or something else fancy.
+
+*/
+
+//Redis Settings
+#define RD_HOST localhost
+#define RD_AUTH LetsMakeSomeAntsDoWorkForUs
+
+//Postgres Settings
+#define PG_HOST localhost
+#define PG_PORT 5432
+#define PG_DB acs44
+#define PG_USER acs44user
+#define PG_PASS LetsMakeSomeAntsDoWorkForUs
